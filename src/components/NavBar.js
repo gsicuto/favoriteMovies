fad900f37571c804f09ffda9ab23020f240ac05c
D-Kunrath <dkunrath@gmail.com>
2022-02-03 18:44:38 -0300

    add style to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 export const NavBar = () => {
-  return <nav style={
-      {
-        display:'flex', 
-        justifyContent:'space-between',
-        height:'30px',
-        backgroundColor:'steelblue'
-      }}> 
-    <NavLink to='/'><span> LOGO </span></NavLink>
-    <div>
-      <NavLink to='/movies-list'>Listagem</NavLink>
-      <NavLink to='/addmovie'>Adicionar Filmes</NavLink>
-    </div>
-    <span>Filmes Listados - 0</span>
-  </nav>;
+  return (
+    <nav className="navbar">
+      <NavLink to="/" classnName="link">
+        <span className="logo"> logo </span>
+      </NavLink>
+      <div className="nav-links">
+        <NavLink to="/movies-list" className="link">
+          Listagem
+        </NavLink>
+        <NavLink to="/addmovie" className="link">
+          Adicionar Filmes
+        </NavLink>
+      </div>
+      <span className="movies-total">Filmes Listados - 0</span>
+    </nav>
+  );
 };
