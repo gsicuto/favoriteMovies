3832e82bd8aafc13e36014fbd6b89851a4656b7a
Gabriel Sicuto <gabrielsicuto@gmail.com>
2022-02-03 19:49:49 -0300

    include Route on App file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { NavBar } from "./components/NavBar";
+import {Route, Routes} from 'react-router-dom'
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
