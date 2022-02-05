aa430fd643d453d2c97ea7053fdd677249ea940d
Gabriel Sicuto <gabrielsicuto@gmail.com>
2022-02-05 11:00:10 -0300

    added alert component on form

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import { Alert } from './Alert';
 
 export const MovieForm = () => {
 
@@ -11,41 +12,53 @@ export const MovieForm = () => {
   const [genero, setGenero] = useState('')
   const [image, setImage] = useState('')
 
+  const[alert, setAlert] = useState('')
+
   const createMovie = async (e) => {
     e.preventDefault();
 
-    const newMovie = {
-      name,
-      ano,
-      sinopse,
-      score: Number(score),
-      genero: genero.split(',').map(e => e.trim()),
-      image
+    if(name === ''){
+      setAlert('Nome do Filme não pode estar vazio')
+      setTimeout(()=>{ setAlert('')}, 2000)
     }
-    await axios.post('http://localhost:8000/movies', newMovie);
+    else{
+      const newMovie = {
+        name,
+        ano,
+        sinopse,
+        score: Number(score),
+        genero: genero.split(',').map(e => e.trim()),
+        image
+      }
+      await axios.post('http://localhost:8000/movies', newMovie);
+    }
+    
   }
 
   return (
-    <form onSubmit={createMovie}>
-      <input type='text' placeholder='ImageURL' value={image} 
-        onChange={({ target:{ value } }) => setImage(value)}
-      />
-      <input type='text' placeholder='Movie Name' value={name} 
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input type='text' placeholder='Release Year' value={ano} 
-        onChange={(e)=> setAno(e.target.value)}
-      />
-      <input type='text' placeholder='Genres separated with ","' value={genero} 
-        onChange={({ target: { value }})=> setGenero(value)}
-      />
-      <textarea placeholder='Sinopses' value={sinopse} 
-        onChange={({target})=> setSinopse(target.value)}
-      ></textarea>
-      <input type='number' min='0' max='5' placeholder='Score of movie' value={score} 
-        onChange={(e)=> setScore(e.target.value)}
-      />
-      {/* <input type='button' value='SAVE' onClick={}/> */}
-      <button type='submit'>Save</button>
-    </form>);
+    <>
+      {alert !== '' ? <Alert>{alert}</Alert> : null}
+      <form onSubmit={createMovie}>
+        <input type='text' placeholder='ImageURL' value={image}
+          onChange={({ target:{ value } }) => setImage(value)}
+        />
+        <input type='text' placeholder='Movie Name' value={name} 
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type='text' placeholder='Release Year' value={ano} 
+          onChange={(e)=> setAno(e.target.value)}
+        />
+        <input type='text' placeholder='Genres separated with ","' value={genero} 
+          onChange={({ target: { value }})=> setGenero(value)}
+        />
+        <textarea placeholder='Sinopses' value={sinopse} 
+          onChange={({target})=> setSinopse(target.value)}
+        ></textarea>
+        <input type='number' min='0' max='5' placeholder='Score of movie' value={score} 
+          onChange={(e)=> setScore(e.target.value)}
+        />
+        {/* <input type='button' value='SAVE' onClick={}/> */}
+        <button type='submit'>Save</button>
+      </form>
+    </>);
 };
